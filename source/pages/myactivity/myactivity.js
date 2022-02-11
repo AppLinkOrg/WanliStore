665c32a6ce7dabcd99efc0233647fcf0c40da66b
z121y225 <3067169550@qq.com>
2022-02-11 18:59:18 +0800

    2.11

---
+++ source/pages/myactivity/myactivity.js
@@ -0,0 +1,48 @@
+// pages/goodsinfo/goodsinfo.js
+// pages/mall/mall.js
+import {
+    AppBase
+  } from "../../appbase";
+  import {
+    ApiConfig
+  } from "../../apis/apiconfig";
+  import {
+    InstApi
+  } from "../../apis/inst.api.js";
+  import {
+    MallApi
+  } from "../../apis/mall.api.js";
+  
+  class Content extends AppBase {
+    constructor() {
+      super();
+    }
+    onLoad(options) {
+      this.Base.Page = this;
+      wx.setNavigationBarTitle({
+        title: "我的活动"
+      })
+      super.onLoad(options);
+      this.Base.setMyData({
+        nowindex: 1,
+        overlay: true,
+        specificationsinfo: null,
+        number: 1
+      })
+    }
+    onMyShow() {
+      var that = this;
+      var mallapi = new MallApi();
+
+  
+    }
+
+  
+  }
+  
+  var content = new Content();
+  var body = content.generateBodyJson();
+  body.onLoad = content.onLoad;
+  body.onMyShow = content.onMyShow;
+
+  Page(body)
