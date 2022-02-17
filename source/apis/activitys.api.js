504561712a06a0b93d3c2800a6d31140e4dd9a87
z121y225 <3067169550@qq.com>
2022-02-17 18:29:33 +0800

    2.17

--- source/apis/activitys.api.js
+++ source/apis/activitys.api.js
@@ -297,6 +297,38 @@ export class ActivitysApi{
         })
     }
 
+    updateactivitystatus(json, callback, showLoading = true) {
+
+        if (showLoading)
+            ApiConfig.ShowLoading();
+
+        var header = ApiConfig.GetHeader();
+        console.log(header);
+        console.log(json);
+        wx.request({
+            url: ApiConfig.GetApiUrl() + 'activitys/updateactivitystatus',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+                if (callback != null) {
+                    callback(res.data);
+                }
+            },
+            fail: function (res) {
+                console.log(res);
+                callback(false);
+            },
+            complete: function (res) {
+                console.log(res);
+            
+                if (showLoading)
+                    ApiConfig.CloseLoading();
+            }
+        })
+    }
+
     uploadactivityinfo(json, callback, showLoading = true) {
 
         if (showLoading)
