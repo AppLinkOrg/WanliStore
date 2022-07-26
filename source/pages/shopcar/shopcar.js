e21984918a25cbf02627470441e1af5f29c37b8f
Mudust <1059821956@qq.com>
2022-07-26 22:46:16 +0800

    7.26

--- source/pages/shopcar/shopcar.js
+++ source/pages/shopcar/shopcar.js
@@ -22,25 +22,30 @@ class Content extends AppBase {
   }
   onLoad(options) {
     this.Base.Page = this;
+    this.Base.setMyData({
+      // 商品数量
+      quantity: 1,
 
+
+    })
     super.onLoad(options);
     var that = this;
     var shopcart = new shopcartlist();
     var mallapi = new MallApi();
     shopcart.shopcartlist({}, res => {
-      console.log(res,'我是第一');
+      console.log(res, '我是第一');
       let newArr = [];
       for (let i = 0; i < res.length; i++) {
         mallapi.goodsinfo({
           id: res[i].goods_id
         }, (info) => {
-        // //  newArr.push(info)
-        res[i].list = info;
+          // //  newArr.push(info)
+          res[i].list = info;
         });
       }
       that.Base.setMyData({
         // infoList:newArr,
-        shoCartList:res
+        shoCartList: res
       })
     })
 
@@ -48,11 +53,40 @@ class Content extends AppBase {
   onMyShow() {
     var that = this;
 
-   
+
 
   }
   checkboxChange(e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  }
+
+  // 增加数量按钮
+  addquantity(e) {
+    let quantity = this.Base.getMyData().quantity;
+    console.log(quantity);
+    quantity++;
+    this.Base.setMyData({
+      quantity
+    })
+  }
+
+  // 减少数量按钮
+  reducequantity(e) {
+    let quantity = this.Base.getMyData().quantity;
+    console.log(quantity);
+    quantity--;
+    this.Base.setMyData({
+      quantity
+    })
+  }
+
+  //输入框编辑数量
+  editInput(e) {
+    console.log(e);
+    let quantity = e.detail.value;
+    this.Base.setMyData({
+      quantity
+    })
   }
 
 }
@@ -62,7 +96,9 @@ var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
 body.checkboxChange = content.checkboxChange;
-
+body.addquantity = content.addquantity;
+body.reducequantity = content.reducequantity;
+body.editInput = content.editInput;
 
 
 Page(body)
